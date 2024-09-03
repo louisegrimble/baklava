c3f7249cd175ed481ffb90b31c22281ddec3f953
Louise Grimble <louiseegrimble@gmail.com>
2024-09-03 14:02:32 +0100

    first commit

---
+++ src/baklava/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about baklava."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Louise Grimble"
+__email__ = "louiseegrimble@gmail.com"
+__version__ = "0.1.0"
